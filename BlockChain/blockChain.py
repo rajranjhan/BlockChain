e4720ae9fb89c51b8d56fa3d9a464d540feae231
rajranjhan <ranjhan@hotmail.com>
2015-07-15 17:41:51 -0400

    Nightly

--- BlockChain/blockChain.py
+++ BlockChain/blockChain.py
@@ -1,13 +1,19 @@
 import struct 
 import numpy as np
 import os
+class BlockPrefix(object): #Must be new-style class!
+    #slots save memory in 
+    __slots__ = ['version', 'previousBlock', 'merkleRoot', 'timeStamp', 'bits', 'nonce']
 
-class blockPrefix(object):
-    __slots__ = ('version', 'previousBlock', 'merkleRoot', 'timeStamp', 'bits', 'nonce')
-
-class blockChain(object):
+class BlockChain(object):
     """Parses a blockchain"""
+    #Constants
+    BLOCK_LEN_BYTES = 4
+    
+    #properties
     _filename = ""
+    _currentFilePos = 0
+
 
     def loadfile(_self,file):
         _filename = file;
@@ -16,19 +22,35 @@ class blockChain(object):
         if not _filename:
             return
         # TODO find next block in file
-        yield block
+        while True:
+            (_currentFilePos, block) = getBlock(_filename, _currentFilePos)
+            if(_currentFilePos != -1) :
+                yield block
+            else:
+                break
 
-
-    def parseBlock(_self, blockChainFile, position=0):
+    def getBlock(_self, blockChainFile, position=0):
         with open(blockChainFile, "rb") as file:
             file.seek(position, os.SEEK_SET)            
-            byte = hex(int.from_bytes(file.read(4), 'little')).upper()           
+            byte = hex(int.from_bytes(file.read(4), 'little')).upper()    
             if(byte == '0XD9B4BEF9'):
-                print("Found Start")
-                     
+                #read number of bytes in the block.  
+                # unpack returns a tuple of 
+                # '<' = little endian, 'i' = integer
+                blockLength, = struct.unpack('<i', file.read(_self.BLOCK_LEN_BYTES))
+                blockPrefix = BlockPrefix()
+                block = file.read(blockLength)
+                return block
+
+     def parseBlock(_self, block):                      
+                blockPrefix = BlockPrefix()
+                blockPrefix.version, = struct.unpack('<i', block(_self.BLOCK_LEN_BYTES))
+                return block
+                             
 
 if __name__ == '__main__':
-    blockChain().parseBlock('blk00000.dat')
-
+    blockChain = BlockChain('blk00000.dat')
+    for b in blockChain.blocks:
+        blockPrefix = blockchain.parseBlock(b)
 
 
